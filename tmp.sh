6b4b3cf8a0bf6fbeb0c964aff158f4f264cad08d
yzolkin <yzolkin@scorewarrior.com>
2022-07-05 12:50:53 +0300

    test one more generator

--- tmp.sh
+++ tmp.sh
@@ -4,6 +4,4 @@ kubectl port-forward service/argocd-server -n argocd 8080:443 &
 
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
 
-kubectl -n argocd apply -f argocd-applicationset.Application.yml
-
-kubectl -n argocd apply -f node-exporter.ApplicationSet.yml
+kubectl -n argocd apply -f root.Application.yml
